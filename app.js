ad547ae2da733511f664ed440f70c7a05b1cfe55
Rishi Kumar <rishikumar10dec@gmail.com>
2023-08-29 15:39:07 +0530

    routes and view setup

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const articlesRouter = require('./routes/articles');
 const app = express();
 
+app.set('view engine', 'ejs');
+
+app.use('/articles', articlesRouter);
+
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    const articles = [
+        {
+            title: 'Test',
+            date: Date.now(),
+            description: 'This is the test description'
+        }
+    ]
+    res.render('index', {articles : articles});
 });
 
 app.listen(5000);
