3064a6a4da2ba899d4a18e058096901734407609
Rishi Kumar <rishikumar10dec@gmail.com>
2023-09-02 15:54:48 +0530

    markdown working

--- models/article.js
+++ models/article.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify'); // title in the url
-const marked = require('marked'); // markdown
+const { marked } = require('marked'); // markdown to html
+
+// when adding markdown to change it into html we have to check
+// for malicious html
+const createDomPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+const dompurify = createDomPurify(new JSDOM().window);
 
 const articleSchema = new mongoose.Schema({
     title: {
@@ -22,6 +28,10 @@ const articleSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true
+    },
+    sanitizedHtml: {
+        type: String,
+        required: true
     }
 });
 
@@ -30,6 +40,9 @@ articleSchema.pre('validate', function(next) {
     if(this.title) {
         this.slug = slugify(this.title, { lower: true,
             strict: true });
+    }
+    if(this.markdown) {
+        this.sanitizedHtml = dompurify.sanitize(marked(this.markdown));
     }
     next();
 });
